b5727d5985517cc6bb62e2cee1770a822282e609
Kiryl Sankouski <sankovsky85@gmail.com>
2023-11-06 15:52:43 +0100

    added 2 zad

--- PRG1/Lekcja 9/Zad1/main.cpp
+++ PRG1/Lekcja 9/Zad1/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int pierwszaZmienna, drugaZmienna;
+
+
+    std::cout << "Podaj pierwsza zmienna: ";
+    std::cin >> pierwszaZmienna;
+
+    std::cout << "Podaj druga zmienna: ";
+    std::cin >> drugaZmienna;
+
+
+    int* wskPierwsza = &pierwszaZmienna;
+    int* wskDruga = &drugaZmienna;
+
+
+    int temp = *wskPierwsza;
+    *wskPierwsza = *wskDruga;
+    *wskDruga = temp;
+
+
+    std::cout << "Po zamianie miejscami:" << std::endl;
+    std::cout << "Pierwsza zmienna: " << pierwszaZmienna << std::endl;
+    std::cout << "Druga zmienna: " << drugaZmienna << std::endl;
+
     return 0;
 }
---
+++ PRG1/Lekcja 9/Zad2/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int rozmiar;
+
+
+    cout << "Podaj rozmiar tablicy: ";
+    cin >> rozmiar;
+
+
+    if (rozmiar <= 0) {
+        cout << "Rozmiar tablicy musi być większy od zera." << endl;
+        return 1;
+    }
+
+
+    int* tablica = new int[rozmiar];
+
+
+    srand(static_cast<unsigned>(time(nullptr)));
+
+
+    for (int i = 0; i < rozmiar; i++) {
+        tablica[i] = rand() % 100;
+    }
+
+
+    int* minElement = &tablica[0];
+    int* maxElement = &tablica[0];
+    int suma = tablica[0];
+
+    for (int i = 1; i < rozmiar; i++) {
+        if (tablica[i] < *minElement) {
+            minElement = &tablica[i];
+        }
+        if (tablica[i] > *maxElement) {
+            maxElement = &tablica[i];
+        }
+        suma += tablica[i];
+    }
+
+
+    cout << "Tablica losowych liczb:" << endl;
+    for (int i = 0; i < rozmiar; i++) {
+        cout << tablica[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Najmniejszy element: " << *minElement << endl;
+    cout << "Największy element: " << *maxElement << endl;
+    cout << "Suma wszystkich elementów: " << suma << endl;
+
+
+    delete[] tablica;
+
+    return 0;
+}
